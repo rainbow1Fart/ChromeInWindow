4f18d8a5bc5dc87147b65c606cc59b45b426da26
zhuhanlin <30095174@qq.com>
2022-01-24 15:54:29 +0800

    添加项目文件。

---
+++ App.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+using System;
+using System.Runtime.CompilerServices;
+using CefSharp;
+using System.IO;
+using System.Reflection;
+using System.Windows.Threading;
+
+namespace ChromeInCore
+{
+    /// <summary>
+    /// App.xaml 的交互逻辑
+    /// </summary>
+    public partial class App : Application
+    {
+
+    }
+}
---
+++ OpenPageSelf.cs
@@ -0,0 +1,47 @@
+﻿using CefSharp;
+using CefSharp.Wpf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChromeInCore
+{
+    internal class OpenPageSelf : ILifeSpanHandler
+    {
+        public bool DoClose(IWebBrowser chromiumWebBrowser, IBrowser browser)
+        {
+            return false;
+        }
+
+        public void OnAfterCreated(IWebBrowser chromiumWebBrowser, IBrowser browser)
+        {
+            ;
+        }
+
+        public void OnBeforeClose(IWebBrowser chromiumWebBrowser, IBrowser browser)
+        {
+            ;
+        }
+
+        public bool OnBeforePopup(IWebBrowser chromiumWebBrowser, 
+            IBrowser browser, 
+            IFrame frame, 
+            string targetUrl, 
+            string targetFrameName,
+            WindowOpenDisposition targetDisposition, 
+            bool userGesture, 
+            IPopupFeatures popupFeatures, 
+            IWindowInfo windowInfo, 
+            IBrowserSettings browserSettings, 
+            ref bool noJavascriptAccess, 
+            out IWebBrowser newBrowser)
+        {
+            newBrowser = null;
+            var browserControl = (ChromiumWebBrowser)chromiumWebBrowser;
+            browserControl.Load(targetUrl);
+            return true; //Return true to cancel the popup creation copyright by codebye.com.
+        }
+    }
+}
